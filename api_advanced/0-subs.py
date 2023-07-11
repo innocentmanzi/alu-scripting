2212ab336999ea7e2feb948663466309b9ae37e3
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2023-07-11 08:53:52 +0000

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,7 +4,7 @@ import requests
 def number_of_subscribers(subreddit):
     req = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
+        headers={"User-Agent": "wintermancer@lapbox"},
     )
 
     if req.status_code == 200:
