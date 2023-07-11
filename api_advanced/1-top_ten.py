f041ae90fcc8fce3a04ee5c08be03278c09f17b4
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2023-07-11 09:03:12 +0000

    task1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-
 import requests
 
 def top_ten(subreddit):
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        
+        for i, post in enumerate(posts[:10], 1):
+            title = post['data']['title']
+            print(f"{i}. {title}")
     else:
-        print(None)
+        print("None")
 
