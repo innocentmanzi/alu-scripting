2ab678f435e758daa8215915c1428a5647853d89
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2023-07-11 09:00:17 +0000

    task1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import requests
+
+def top_ten(subreddit):
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
+
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)
+
