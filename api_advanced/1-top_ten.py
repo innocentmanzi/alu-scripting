8b375d521f26117fab46999e14193fb7963f7630
Innocent MANZI <116680024+innocentmanzi@users.noreply.github.com>
2023-07-11 10:31:32 +0000

    task 1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
@@ -9,7 +14,7 @@ def top_ten(subreddit):
     """
     req = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"},
+        headers={"User-Agent": "Custom"},
         params={"limit": 10},
     )
 
@@ -20,4 +25,3 @@ def top_ten(subreddit):
             print(title)
     else:
         print(None)
-
